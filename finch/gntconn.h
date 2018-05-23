899cacc5554843e5ba6056734e432115c94248f5
Mike Ruprecht <cmaiku@gmail.com>
2018-05-23 15:59:55 -0500

    Drop gnome-keyring keyring
    libgnome-keyring has been deprecated in favor of libsecret. This
    happened for libgnome-keyring 3.12. The version of libsecret released
    at that time was 0.18. Adequate support for libsecret may have been
    available before that.
    
    This does depend on something (gnome-keyring, not to be confused with
    libgnome-keyring) implementing the org.freedesktop.secrets service.
    It appears gnome-keyring has supported this for plenty of time.
    
    Since we have a libsecret keyring and all the distributions we care
    about seem to have a new enough version, this patch removes the
    gnome-keyring keyring.

---
+++ finch/gntconn.h
@@ -0,0 +1,60 @@
+/* finch
+ *
+ * Finch is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ */
+
+#ifndef _GNT_CONN_H
+#define _GNT_CONN_H
+/**
+ * SECTION:gntconn
+ * @section_id: finch-gntconn
+ * @short_description: <filename>gntconn.h</filename>
+ * @title: Connection API
+ */
+
+#include "connection.h"
+
+/**********************************************************************
+ * GNT Connection API
+ **********************************************************************/
+
+/**
+ * finch_connections_get_ui_ops:
+ *
+ * Get the ui-functions.
+ *
+ * Returns: The PurpleConnectionUiOps structure populated with the appropriate functions.
+ */
+PurpleConnectionUiOps *finch_connections_get_ui_ops(void);
+
+/**
+ * finch_connections_init:
+ *
+ * Perform necessary initializations.
+ */
+void finch_connections_init(void);
+
+/**
+ * finch_connections_uninit:
+ *
+ * Perform necessary uninitializations.
+ */
+void finch_connections_uninit(void);
+
+#endif
