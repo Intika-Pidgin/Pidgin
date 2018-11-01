5ab50e82f42b2d0c105a651309072c878db6f319
Mike Ruprecht <cmaiku@gmail.com>
2018-11-01 15:45:13 -0500

    libpurple: Port self-contained code from g_type_class_add_private()
    This patch ports the self-contained libpurple GObjects away from the
    deprecated g_type_class_add_private() function in favor of the
    G_DEFINE_TYPE_WITH_PRIVATE() macro.

---
+++ finch/finch.h
@@ -0,0 +1,47 @@
+/* finch
+ *
+ * Finch is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ */
+
+#ifndef _FINCH_H_
+#define _FINCH_H_
+/**
+ * SECTION:finch
+ * @section_id: finch-finch
+ * @short_description: <filename>finch.h</filename>
+ * @title: UI Definitions and Includes
+ */
+
+#include <glib.h>
+
+#define FINCH_UI "gnt-purple"
+
+#define FINCH_PREFS_ROOT "/finch"
+
+#define FINCH_GET_DATA(obj)        (obj)->ui_data
+#define FINCH_SET_DATA(obj, data)  (obj)->ui_data = data
+
+/**
+ * finch_start:
+ *
+ * Start finch with the given command line arguments.
+ */
+gboolean finch_start(int *argc, char ***argv);
+
+#endif
---
+++ finch/gntprefs.h
@@ -0,0 +1,63 @@
+/* finch
+ *
+ * Finch is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ */
+
+#ifndef _GNT_PREFS_H
+#define _GNT_PREFS_H
+/**
+ * SECTION:gntprefs
+ * @section_id: finch-gntprefs
+ * @short_description: <filename>gntprefs.h</filename>
+ * @title: Preferences API
+ */
+
+/**********************************************************************
+ * GNT Preferences API
+ **********************************************************************/
+
+/**
+ * finch_prefs_init:
+ *
+ * Perform necessary initializations.
+ */
+void finch_prefs_init(void);
+
+/**
+ * finch_prefs_show_all:
+ *
+ * Show the preferences dialog.
+ */
+void finch_prefs_show_all(void);
+
+/**
+ * finch_prefs_show_keyring:
+ *
+ * Show the preferences dialog for the selected keyring.
+ */
+void finch_prefs_show_keyring(void);
+
+/**
+ * finch_prefs_update_old:
+ *
+ * You don't need to know about this.
+ */
+void finch_prefs_update_old(void);
+
+#endif
