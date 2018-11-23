946d1209fbc7913e34bb648d646627f0c7d03ec8
Mike Ruprecht <cmaiku@gmail.com>
2018-11-23 16:45:22 -0600

    irc: Implement URI handler
    This patch implements a URI handler for the IRC prpl. This is based
    on and intended to replace purple-url-handler's implementation.
    However, it doesn't attempt to use a URI to send messages in a chat.
    It seemed like a clunky way of doing it and I couldn't find any
    standard referring to it. It can be re-added later if need be.

---
+++ finch/gntstatus.h
@@ -0,0 +1,53 @@
+/* finch
+ *
+ * Finch is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ */
+
+#ifndef _GNT_STATUS_H
+#define _GNT_STATUS_H
+/**
+ * SECTION:gntstatus
+ * @section_id: finch-gntstatus
+ * @short_description: <filename>gntstatus.h</filename>
+ * @title: Status API
+ */
+
+#include <status.h>
+#include <savedstatuses.h>
+
+/**********************************************************************
+ * GNT BuddyList API
+ **********************************************************************/
+
+/**
+ * finch_savedstatus_show_all:
+ *
+ * Show a dialog with all the saved statuses.
+ */
+void finch_savedstatus_show_all(void);
+
+/**
+ * finch_savedstatus_edit:
+ * @saved: The saved status to edit. Set it to %NULL to create a new status.
+ *
+ * Show a dialog to edit a status.
+ */
+void finch_savedstatus_edit(PurpleSavedStatus *saved);
+
+#endif
