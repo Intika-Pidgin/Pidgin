a0b138f3e958983cb2a92214d175977c44c99a70
Fabrice Bellet <fabrice@bellet.info>
2019-06-26 09:33:38 +0200

    fix coding style in previous assert macros commit

--- libpurple/plugins/ssl/ssl-gnutls.c
+++ libpurple/plugins/ssl/ssl-gnutls.c
@@ -712,12 +712,13 @@ x509_import_from_file(const gchar * filename)
 	/* Next, we'll simply yank the entire contents of the file
 	   into memory */
 	/* TODO: Should I worry about very large files here? */
-	if (g_file_get_contents(filename,
+	if (!g_file_get_contents(filename,
 			&buf,
 			&buf_sz,
 			NULL      /* No error checking for now */
-		) == FALSE)
+			)) {
 		return NULL;
+	}
 
 	/* Load the datum struct */
 	dt.data = (unsigned char *) buf;
