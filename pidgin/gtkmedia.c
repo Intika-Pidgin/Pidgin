c25239830c8e6d0e351ab8996bf3dd0fadbffcb2
Fabrice Bellet <fabrice@bellet.info>
2019-07-04 13:31:03 +0200

    gtkmedia: keep a reference on the media widget in the realize callback

--- pidgin/gtkmedia.c
+++ pidgin/gtkmedia.c
@@ -584,6 +584,7 @@ realize_cb_cb(PidginMediaRealizeData *data)
 
 	g_free(data->session_id);
 	g_free(data->participant);
+	g_object_unref(data->gtkmedia);
 	g_free(data);
 	return FALSE;
 }
@@ -960,7 +961,7 @@ pidgin_media_ready_cb(PurpleMedia *media, PidginMedia *gtkmedia, const gchar *si
 		gtk_box_pack_start(GTK_BOX(recv_widget), aspect, TRUE, TRUE, 0);
 
 		data = g_new0(PidginMediaRealizeData, 1);
-		data->gtkmedia = gtkmedia;
+		data->gtkmedia = g_object_ref(gtkmedia);
 		data->session_id = g_strdup(sid);
 		data->participant = g_strdup(gtkmedia->priv->screenname);
 
@@ -991,7 +992,7 @@ pidgin_media_ready_cb(PurpleMedia *media, PidginMedia *gtkmedia, const gchar *si
 		gtk_box_pack_start(GTK_BOX(send_widget), aspect, FALSE, TRUE, 0);
 
 		data = g_new0(PidginMediaRealizeData, 1);
-		data->gtkmedia = gtkmedia;
+		data->gtkmedia = g_object_ref(gtkmedia);
 		data->session_id = g_strdup(sid);
 		data->participant = NULL;
 
