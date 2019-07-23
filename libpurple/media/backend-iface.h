62c90ce33571d22ef2a803208c56e7940fe0627d
Fabrice Bellet <fabrice@bellet.info>
2019-07-23 12:47:17 +0200

    More fixes from pull request #523 review

--- libpurple/media/backend-iface.h
+++ libpurple/media/backend-iface.h
@@ -252,7 +252,7 @@ gboolean purple_media_backend_set_decryption_parameters(PurpleMediaBackend *self
  * @param self The media object to find the session in.
  * @param sess_id The id of the session to set parameters of.
  * @param participant The participant of the session to set parameters of.
- * @param require-encryption TRUE if the media requires encryption.
+ * @param require_encryption TRUE if the media requires encryption.
  *
  * @since 2.14.0
  */
