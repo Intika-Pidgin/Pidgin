9282865ba69751bfafe3f55db736ad51447f17da
Elliott Sales de Andrade <qulogic@pidgin.im>
2019-09-02 22:48:24 -0400

    Use standard legalese in XMPP console.

--- pidgin/plugins/xmppconsole/xmppconsole.c
+++ pidgin/plugins/xmppconsole/xmppconsole.c
@@ -1,7 +1,9 @@
 /*
  * Purple - XMPP debugging tool
  *
- * Copyright (C) 2002-2003, Sean Egan
+ * Pidgin is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +17,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301 USA
  *
  */
 
