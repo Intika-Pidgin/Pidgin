4e9f5ff4710bc6972cb7caf1a47776556eb4f9e2
Gary Kramlich <grim@reaperworld.com>
2019-09-03 23:57:12 -0500

    Add a COPYRIGHT header and remove some extra whitespace

--- libpurple/plugins/purple-toast.c
+++ libpurple/plugins/purple-toast.c
@@ -1,3 +1,23 @@
+/* pidgin
+ *
+ * Pidgin is the legal property of its developers, whose names are too numerous
+ * to list here.  Please refer to the COPYRIGHT file distributed with this
+ * source distribution.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ */
 #include <glib.h>
 #include <gmodule.h>
 #include <gplugin.h>
@@ -76,7 +96,7 @@ _purple_toast_find_icon(PurpleAccount *account,
 		const gchar *path = NULL;
 
 		image = purple_buddy_icons_find_account_icon(account);
-		if(PURPLE_IS_IMAGE(image)) {		
+		if(PURPLE_IS_IMAGE(image)) {
 			path = purple_image_get_path(image);
 			g_message("toast path: %p", path);
 			if(path) {
@@ -89,7 +109,6 @@ _purple_toast_find_icon(PurpleAccount *account,
 		}
 
 		if(!G_IS_ICON(icon)) {
-			
 		}
 	}
 
