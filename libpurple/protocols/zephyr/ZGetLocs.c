bd9797a2849fab5abd481e568bcf3050a6713383
Elliott Sales de Andrade <qulogic@pidgin.im>
2019-09-16 20:47:19 -0400

    Fix misleading indentation warnings.

--- libpurple/protocols/zephyr/ZGetLocs.c
+++ libpurple/protocols/zephyr/ZGetLocs.c
@@ -15,22 +15,25 @@ Code_t ZGetLocations(ZLocations_t *location, int *numlocs);
 
 Code_t ZGetLocations(ZLocations_t *location, int *numlocs)
 {
-    int i;
+	int i;
 
-    if (!__locate_list)
-	return (ZERR_NOLOCATIONS);
+	if (!__locate_list) {
+		return (ZERR_NOLOCATIONS);
+	}
 
-    if (__locate_next == __locate_num)
-	return (ZERR_NOMORELOCS);
+	if (__locate_next == __locate_num) {
+		return (ZERR_NOMORELOCS);
+	}
 
 	for (i = 0; i < MIN(*numlocs, __locate_num - __locate_next); i++) {
 		location[i] = __locate_list[i + __locate_next];
 	}
 
-    if (__locate_num-__locate_next < *numlocs)
-	*numlocs = __locate_num-__locate_next;
+	if (__locate_num - __locate_next < *numlocs) {
+		*numlocs = __locate_num - __locate_next;
+	}
 
-    __locate_next += *numlocs;
+	__locate_next += *numlocs;
 
-    return (ZERR_NONE);
+	return ZERR_NONE;
 }
--- libpurple/protocols/zephyr/ZGetSubs.c
+++ libpurple/protocols/zephyr/ZGetSubs.c
@@ -15,23 +15,26 @@ Code_t ZGetSubscriptions(ZSubscription_t *subscription, int *numsubs);
 
 Code_t ZGetSubscriptions(ZSubscription_t *subscription, int *numsubs)
 {
-    int i;
+	int i;
 
-    if (!__subscriptions_list)
-	return (ZERR_NOSUBSCRIPTIONS);
+	if (!__subscriptions_list) {
+		return (ZERR_NOSUBSCRIPTIONS);
+	}
 
-    if (__subscriptions_next == __subscriptions_num)
-	return (ZERR_NOMORESUBSCRIPTIONS);
+	if (__subscriptions_next == __subscriptions_num) {
+		return (ZERR_NOMORESUBSCRIPTIONS);
+	}
 
 	for (i = 0; i < MIN(*numsubs, __subscriptions_num - __subscriptions_next);
 	     i++) {
 		subscription[i] = __subscriptions_list[i + __subscriptions_next];
 	}
 
-    if (__subscriptions_num-__subscriptions_next < *numsubs)
-	*numsubs = __subscriptions_num-__subscriptions_next;
+	if (__subscriptions_num - __subscriptions_next < *numsubs) {
+		*numsubs = __subscriptions_num - __subscriptions_next;
+	}
 
-    __subscriptions_next += *numsubs;
+	__subscriptions_next += *numsubs;
 
-    return (ZERR_NONE);
+	return ZERR_NONE;
 }
